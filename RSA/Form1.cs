866f3165e05e590b39fa1adaa2c70e62c7fdce1a
kodmonzoltal <komdon.zoltal@gmail.com>
2020-05-25 22:47:24 +0200

    githuboskód
    Amit küldtél kódót gtihubról, azt kezdtem el megírni. A metódusok megvannak már csak a felhasználása kell. Meg sztem írjunk hozzá egy dokumentációt ahol leírjuk, hogy melyik metódus mit csinál pontsan, csak nem tudom honnan lehetne ezeket kikeresni de majd még utána nézek :D

--- RSA/Form1.cs
+++ RSA/Form1.cs
@@ -30,43 +30,10 @@ namespace RSA
             btn_kodol.BackColor = Color.FromArgb(125, Color.White);
             label1.BackColor = Color.FromArgb(125, Color.White);
             label2.BackColor = Color.FromArgb(125, Color.White);
+            
         }
 
-        static public byte[] Encryption(byte [] data, RSAParameters RSAKey, bool DoOAEPPadding)
-        {
-            try
-            {
-                byte[] encryptedDAta;
-                using(RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-                {
-                    RSA.ImportParameters(RSAKey);
-                    encryptedDAta = RSA.Encrypt(data, DoOAEPPadding);
-                }
-                return encryptedDAta;
-            }
-            catch(CryptographicException e)
-            {
-                return null;
-            }
-        }
-
-        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
-        {
-            try
-            {
-                byte[] decryptedData;
-                using(RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-                {
-                    RSA.ImportParameters(RSAKey);
-                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
-                }
-                return decryptedData;
-            }
-            catch(CryptographicException e)
-            {
-                return null;
-            }
-        }
+        
 
         private void Btn_exit_Click(object sender, EventArgs e)
         {
@@ -80,15 +47,77 @@ namespace RSA
 
         private void Btn_kodol_Click(object sender, EventArgs e)
         {
-            plaintext = ByteConverter.GetBytes(richTextBox1.Text);
-            encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
-            richTextBox2.Text = ByteConverter.GetString(encryptedtext);
+            //plaintext = ByteConverter.GetBytes(richTextBox1.Text);
+            //encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+            //richTextBox2.Text = ByteConverter.GetString(encryptedtext);
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            byte[] decrpytedtext = Decryption(encryptedtext, RSA.ExportParameters(true), false);
-            richTextBox1.Text = ByteConverter.GetString(decrpytedtext);
+            //byte[] decrpytedtext = Decryption(encryptedtext, RSA.ExportParameters(true), false);
+            //richTextBox1.Text = ByteConverter.GetString(decrpytedtext);
+        }
+
+        public static string GetKeyString(RSAParameters publicKey)
+        {
+            var stringWriter = new System.IO.StringWriter();
+            var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            xmlSerializer.Serialize(stringWriter, publicKey);
+            return stringWriter.ToString();
+        }
+
+        public static string Encrypt(string textToEncrypt, string publicKeyString)
+        {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes(textToEncrypt);
+
+            using(var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    rsa.FromXmlString(publicKeyString.ToString());
+                    var encryptedData = rsa.Encrypt(bytesToEncrypt, true);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        public static string Decrypt(string textToDecrypt, string privateKeyString)
+        {
+            var bytesToDecrypt = Encoding.UTF8.GetBytes(textToDecrypt);
+            using(var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    rsa.FromXmlString(privateKeyString);
+                    var resultBytes = Convert.FromBase64String(textToDecrypt);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        private static string GenerateTestString()
+        {
+            Guid opportunityId = Guid.NewGuid();
+            Guid systemUserId = Guid.NewGuid();
+            string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("opportunityid={0}", opportunityId.ToString());
+            sb.AppendFormat("&systemuserid={0}", systemUserId.ToString());
+            sb.AppendFormat("&currenttime={0}", currentTime);
+
+            return sb.ToString();
         }
     }
 }
---
+++ RSA/RSA.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    class RSA
+    {
+        Random rnd = new Random();
+        
+        public double RelativPrim() //i a kisebb, modulus a nagyobb
+        {
+            double i = 1;
+            double lnko = 0;
+            double modulus = Modulus();
+            for ( i = 1; i < modulus; i++)
+            {
+
+                for (double j = 1; j < i; j++)
+                {
+                    if (i % j == 0 && modulus % j == 0)
+                    {
+                        lnko = j;
+                        Console.WriteLine(lnko);
+                        if (lnko == 1)
+                            break;
+                    }
+                }
+                
+            }
+            return i;
+        }
+
+        public double Modulus()
+        {
+            double p;
+            double q;
+            do
+            {
+                p = rnd.Next(50, 150);
+            }
+            while (PrimCheck(p) != true);
+            do
+            {
+                q = rnd.Next(50, 150);
+            }
+            while (PrimCheck(q) != true);
+            Console.WriteLine(p);
+            Console.WriteLine(q);
+            return p * q;
+        }
+
+        public bool PrimCheck(double num)
+        {
+            for (int i = 2; i <= num/2; i++)
+            {
+                if(num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
